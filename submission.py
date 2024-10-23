513130eed0b48cd5963f84c56f86f9ade5e046aa
Jason R <jay.0408@gmail.com>
2024-10-23 15:32:34 -0500

    None

--- submission.py
+++ submission.py
@@ -7,4 +7,4 @@ def write_agent_to_file(function, file):
         print(function, "written to", file)
 
 write_agent_to_file(my_agent, "submission.py")
-<function my_agent at 0x7f53c74c68c8> written to submission.py
+#<function my_agent at 0x7f53c74c68c8> written to submission.py
